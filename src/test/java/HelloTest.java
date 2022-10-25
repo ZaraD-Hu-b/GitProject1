85d019467483660ca54984646bd0a82b6c9a37a6
Zara <zama.ny1974@gmail.com>
2022-10-24 21:09:51 -0400

    work 2 done

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,12 @@
+package test.java;
+
+public class HelloTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Test");
+        //work 1
+
+        //work 2
+    }
+}
