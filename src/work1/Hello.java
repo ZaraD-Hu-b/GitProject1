400cdd71628529fd1dcbc92540aaa54d9405577c
Zara <zama.ny1974@gmail.com>
2022-11-13 10:40:49 -0500

    added more work

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 
         System.out.println("Work1 started!");
         System.out.println("Created git repository");
-
+        System.out.println("Made more commit");
     }
 
 }
